6e9dd02929bb0fca50c1cca74cc9d26a4506e08a
sakusdev <105345796+sakusdev@users.noreply.github.com>
2026-08-14 08:11:06 +0900

    Populate gameplay exports

--- crates/ferrum-game/src/exports.rs
+++ crates/ferrum-game/src/exports.rs
@@ -1,1 +1,12 @@
 //! Crate-root re-exports for ferrum-game.
+pub use crate::attributes::{Attribute,AttributeError,AttributeMap,AttributeModifier,AttributeOperation};
+pub use crate::command::{CommandError,CommandOutcome,CommandSource,GameCommand,execute_command,parse_command};
+pub use crate::container::{ContainerClick,ContainerClickKind,ContainerError,ContainerMutation,ContainerSnapshot,InventorySession,MAX_CONTAINER_SLOTS,OUTSIDE_SLOT,PLAYER_CONTAINER_ID};
+pub use crate::crafting::{CraftingError,CraftingGrid,Ingredient,ShapelessRecipe};
+pub use crate::entity::{Entity,EntityError,EntityId,EntityStore,EntityType,EntityUuid,Transform,Velocity};
+pub use crate::inventory::{EquipmentSlot,HOTBAR_END,HOTBAR_SLOTS,HOTBAR_START,Inventory,InventoryError,ItemStack,MAIN_INVENTORY_END,MAIN_INVENTORY_START,MAX_VANILLA_STACK_SIZE,OFFHAND_SLOT,PLAYER_INVENTORY_SLOTS};
+pub use crate::item_entity::{DEFAULT_ITEM_PICKUP_DELAY_TICKS,ITEM_ENTITY_AIR_DRAG,ITEM_ENTITY_DATA_KEY,ITEM_ENTITY_GRAVITY_PER_TICK,ITEM_ENTITY_GROUND_DRAG,ITEM_ENTITY_LIFETIME_TICKS,ITEM_ENTITY_VERTICAL_GROUND_DRAG,ITEM_MERGE_RANGE,ITEM_PICKUP_RANGE,ItemEntityData,ItemEntityError,ItemMergeResult,ItemPickupResult,item_entity_data,item_entity_in_pickup_range,merge_nearby_item_entities,set_item_entity_data,spawn_item_entity,spawn_item_entity_with_default_delay,tick_item_entities,try_merge_item_entities,try_pickup_item_entity};
+pub use crate::persistence::{GameSnapshot,PersistenceError};
+pub use crate::player::{Abilities,Difficulty,Experience,GameMode,PlayerError,PlayerState,PlayerUuid,Vitals};
+pub use crate::status_effect::{EffectUpdate,StatusEffect,StatusEffectError,StatusEffectStore,MAX_EFFECT_AMPLIFIER,MAX_EFFECT_DURATION_TICKS};
+pub use crate::state::{GameEvent,GameRuleValue,GameState,GameStateError,GameTime};
